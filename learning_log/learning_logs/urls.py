6f9e16ce115a00ae8aeafb63074b7d7b344774b9
AndriiLevin <andriilevin@gmail.com>
2023-03-30 21:48:40 +0100

    adding topic.html page

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,7 +6,8 @@ app_name = "learning_logs"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 
 
 ]
